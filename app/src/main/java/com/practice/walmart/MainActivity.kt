c74b27d615e23929761d7f571d58130503900779
murtazakhan <murtazahotaki@yahoo.com>
2024-02-15 00:17:29 -0500

    Done as per instruction

--- app/src/main/java/com/practice/walmart/MainActivity.kt
+++ app/src/main/java/com/practice/walmart/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.practice.walmart
 
+
 import CountryAdapter
 import CountryViewModel
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.practice.walmart.R
 import com.practice.walmart.databinding.ActivityMainBinding
 import com.practice.walmart.model.Country
 
 
+
+import java.util.*
+
 class MainActivity : AppCompatActivity() {
-    private var viewModel: CountryViewModel = CountryViewModel()
-
+    private lateinit var viewModel: CountryViewModel
     private lateinit var activityMainBinding: ActivityMainBinding
-
     private lateinit var countryAdapter: CountryAdapter
     private val countryList: MutableList<Country> = mutableListOf()
-
-    private var currentPage = 1
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,37 +43,88 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
         setupRecyclerView()
-
-
-
-        getCountries()
+        observeCountries()
+        setupSearchListener()
+        setupSearchIconClickListener()
+        activityMainBinding.editTextSearch.visibility = View.GONE
     }
 
     private fun setupRecyclerView() {
         activityMainBinding.tvShowRecyclerView.setHasFixedSize(true)
-        countryAdapter = CountryAdapter(
-            this, countryList
-        )
+        countryAdapter = CountryAdapter(this, countryList)
         activityMainBinding.tvShowRecyclerView.adapter = countryAdapter
     }
 
-    private fun getCountries() {
-        toggleLoading()
+    private fun observeCountries() {
         viewModel.countries.observe(this, Observer { countries ->
-            toggleLoading()
             countries?.let { shows ->
+                countryList.clear()
                 countryList.addAll(shows)
                 countryAdapter.notifyDataSetChanged()
-                //showToast("Showing")
             }
         })
     }
 
-    private fun toggleLoading() {
-        if (currentPage == 1) {
-            activityMainBinding.isloading = activityMainBinding.isloading != true
-        } else {
-            activityMainBinding.isloadingMore = activityMainBinding.isloadingMore != true
+    private fun setupSearchListener() {
+        activityMainBinding.editTextSearch.visibility = View.VISIBLE
+        activityMainBinding.editTextSearch.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val searchText = s.toString().toLowerCase(Locale.getDefault())
+
+                Log.d("Search", "Search Text: $searchText")
+                val filteredList = if (searchText.isNotEmpty()) {
+                    val filtered = countryList.filter { country ->
+                        // Check if either the country name or capital contains the search text
+                        country.name.toLowerCase(Locale.getDefault()).contains(searchText) ||
+                                country.capital.toLowerCase(Locale.getDefault()).contains(searchText)
+                    }
+                    // Add debug statement to check the filtered list
+                    Log.d("Search", "Filtered List: $filtered")
+                    filtered
+                } else {
+                    countryList
+                }
+                // Add debug statement to check the size of filtered list
+                Log.d("Search", "Filtered List Size: ${filteredList.size}")
+                countryAdapter.filterList(filteredList)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not used
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not used
+            }
+        })
+    }
+
+    private fun setupSearchIconClickListener() {
+        activityMainBinding.imageViewSearch.setOnClickListener {
+            // Toggle visibility of EditText and hide TextView and ImageView
+            activityMainBinding.editTextSearch.visibility = View.VISIBLE
+            activityMainBinding.textViewCountriesList.visibility = View.GONE
+            activityMainBinding.imageViewSearch.visibility = View.GONE
+            activityMainBinding.closeSearch.visibility = View.VISIBLE
+            activityMainBinding.editTextSearch.requestFocus()
+
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(activityMainBinding.editTextSearch, InputMethodManager.SHOW_IMPLICIT)
+    }
+        activityMainBinding.closeSearch.setOnClickListener{
+            activityMainBinding.editTextSearch.visibility = View.GONE
+            activityMainBinding.textViewCountriesList.visibility = View.VISIBLE
+            activityMainBinding.imageViewSearch.visibility = View.VISIBLE
+            activityMainBinding.closeSearch.visibility = View.GONE
+
+        }
+
+        activityMainBinding.editTextSearch.setOnClickListener {
+            // Toggle visibility of EditText and show TextView and ImageView
+            activityMainBinding.editTextSearch.visibility = View.GONE
+            activityMainBinding.textViewCountriesList.visibility = View.VISIBLE
+            activityMainBinding.imageViewSearch.visibility = View.VISIBLE
+            activityMainBinding.closeSearch.visibility = View.GONE
         }
     }
 }
--- app/src/main/java/com/practice/walmart/adapter/CountryAdapter.kt
+++ app/src/main/java/com/practice/walmart/adapter/CountryAdapter.kt
@@ -1,5 +1,4 @@
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -8,9 +7,10 @@ import com.practice.walmart.model.Country
 import com.practice.walmart.R
 import com.practice.walmart.databinding.CountryListItemBinding
 
-
-class CountryAdapter(private val context: Context, private val countryList: List<Country>) :
+class CountryAdapter(private val context: Context, private var countryList: List<Country>) :
     RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+
+    private var filteredList: List<Country> = countryList
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -21,11 +21,16 @@ class CountryAdapter(private val context: Context, private val countryList: List
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        holder.bind(countryList[position])
+        holder.bind(filteredList[position])
     }
 
     override fun getItemCount(): Int {
-        return countryList.size
+        return filteredList.size
+    }
+
+    fun filterList(filteredList: List<Country>) {
+        this.filteredList = filteredList
+        notifyDataSetChanged()
     }
 
     inner class CountryViewHolder(private val binding: CountryListItemBinding) :
@@ -34,8 +39,6 @@ class CountryAdapter(private val context: Context, private val countryList: List
         fun bind(country: Country) {
             binding.countryModel = country
             binding.executePendingBindings()
-
-
         }
     }
 }
